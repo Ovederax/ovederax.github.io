edb90bccfc0995bc0d42ffbba6b4914b9cd624aa
Ovederax <alekseyblagoy@gmail.com>
2020-08-29 16:14:11 +0600

    add transcribe site

---
+++ translate-json/precache-manifest.8c77a5fb25443ec110c7bad00e2a4459.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "646940ae13ce2804b0729976ac66729f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "397630ed443851321b11",
+    "url": "/static/css/main.0704a261.chunk.css"
+  },
+  {
+    "revision": "7e188f580ff7774d4f22",
+    "url": "/static/js/0.0c13557b.chunk.js"
+  },
+  {
+    "revision": "8f18bf32a43b200fc509",
+    "url": "/static/js/3.93bb6e51.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/3.93bb6e51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "025c7dae4d7b336ef1e8",
+    "url": "/static/js/4.1301fe8e.chunk.js"
+  },
+  {
+    "revision": "397630ed443851321b11",
+    "url": "/static/js/main.c017263d.chunk.js"
+  },
+  {
+    "revision": "4be438d222d6707265a4",
+    "url": "/static/js/runtime-main.36afc3a1.js"
+  }
+]);
